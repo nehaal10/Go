51d72659127b677b646a5bfe4bfe32e33a70284f
Nehaal Reddy <nehaalreddy@Nehaals-MacBook-Pro.local>
2023-07-27 18:51:44 +0530

    hello world

---
+++ main.go
@@ -0,0 +1,7 @@
+package main
+
+import "fmt"
+
+func main() { //main is the entry point just like in c and c++
+	fmt.Println("hello world") //fmt is like iostream in c++ which has all the opration like cout in golant its fmt
+}
